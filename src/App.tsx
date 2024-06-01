fa40cb941a9215af2208f373ba5665e361d63237
Nikita Podberezkin <nikita@brilliant.tech>
2024-06-01 16:59:52 +0300

    Add task description

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * 1. Create form to get user's name, age and hobby.
+ * 2. Display saved information in the table.
+ * 3. Add button and flow to table's entry to edit user's information.
+ * 4. Add button to delete user's information.
+ */
+
 function App() {
   return (
     <div className="App">
